c0be6f2f66fb9e354bfd647ddd7c52aa33c12692
Thomas Bouzy <art.birdiz@gmail.com>
2026-08-28 15:50:14 +0200

    clean project

--- web/app/next-steps/page.jsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-import { DEFAULT_LOCALE } from "../../lib/i18n/locales";
-
-export default function RootNextStepsPage() {
-  redirect(`/${DEFAULT_LOCALE}/next-steps`);
-}
